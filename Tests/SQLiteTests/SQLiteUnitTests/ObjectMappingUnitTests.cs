7bbcf3f90fc59d21d3a2122e272151e7770cfd41
Vedran Bilopavlović <vbilopav@gmail.com>
2019-12-10 15:50:52 +0100

    Add more sqlite tests

--- Tests/SQLiteTests/SQLiteUnitTests/ObjectMappingUnitTests.cs
+++ Tests/SQLiteTests/SQLiteUnitTests/ObjectMappingUnitTests.cs
@@ -23,6 +23,15 @@ namespace SQLiteUnitTests
             public string Bar { get; private set; }
         }
 
+        class TestClass2
+        {
+            public long Id { get; private set; }
+            public string Foo { get; private set; }
+            public DateTime Day { get; private set; }
+            public bool? Bool { get; private set; }
+            public string Bar { get; private set; }
+        }
+
         private const string Query = @"
             with cte(id, foo, day, bool, bar) as (
             select * from (
@@ -64,6 +73,32 @@ namespace SQLiteUnitTests
             Assert.Equal("bar3", result[2].Bar);
         }
 
+        private void AssertTestClass2(IList<TestClass2> result)
+        {
+            Assert.Equal(3, result.Count);
+
+            Assert.Equal(1, result[0].Id);
+            Assert.Equal(2, result[1].Id);
+            Assert.Equal(3, result[2].Id);
+
+            Assert.Equal("foo1", result[0].Foo);
+            Assert.Equal("foo2", result[1].Foo);
+            Assert.Equal("foo3", result[2].Foo);
+
+            Assert.Equal(new DateTime(1977, 5, 19), result[0].Day);
+            Assert.Equal(new DateTime(1978, 5, 19), result[1].Day);
+            Assert.Equal(new DateTime(1979, 5, 19), result[2].Day);
+
+            Assert.Equal(true, result[0].Bool);
+            Assert.Equal(false, result[1].Bool);
+            Assert.Null(result[2].Bool);
+
+            Assert.Null(result[0].Bar);
+            Assert.Equal("bar2", result[1].Bar);
+            Assert.Equal("bar3", result[2].Bar);
+        }
+
+
         [Fact]
         public void SelectMap_Sync()
         {
@@ -81,5 +116,53 @@ namespace SQLiteUnitTests
 
             AssertTestClass(result);
         }
+
+        [Fact]
+        public void SelectMapFromTable_Sync()
+        {
+            using var connection = new SQLiteConnection(fixture.ConnectionString);
+            connection
+                .Execute("create table test_class2 (id integer, foo string, day datetime, bool boolean, bar string);")
+                .Execute(@"
+                    insert into test_class2 
+                    (id, foo, day, bool, bar)
+                    values
+                    (@id1, @foo1, @day1, @bool1, @bar1),
+                    (@id2, @foo2, @day2, @bool2, @bar2),
+                    (@id3, @foo3, @day3, @bool3, @bar3);",
+                    1, "foo1", new DateTime(1977, 5, 19), true, null,
+                    2, "foo2", new DateTime(1978, 5, 19), false, "bar2",
+                    3, "foo3", new DateTime(1979, 5, 19), null, "bar3");
+
+            var result = connection.Read("select * from test_class2").Select<TestClass2>().ToList();
+
+            AssertTestClass2(result);
+
+            connection.Execute("drop table test_class2;");
+        }
+
+        [Fact]
+        public async Task SelectMapFromTable_ASync()
+        {
+            await using var connection = new SQLiteConnection(fixture.ConnectionString);
+            await connection.ExecuteAsync("create table test_class2 (id integer, foo string, day datetime, bool boolean, bar string);"); 
+            await connection.ExecuteAsync(@"
+                    insert into test_class2 
+                    (id, foo, day, bool, bar)
+                    values
+                    (@id1, @foo1, @day1, @bool1, @bar1),
+                    (@id2, @foo2, @day2, @bool2, @bar2),
+                    (@id3, @foo3, @day3, @bool3, @bar3);",
+                    1, "foo1", new DateTime(1977, 5, 19), true, null,
+                    2, "foo2", new DateTime(1978, 5, 19), false, "bar2",
+                    3, "foo3", new DateTime(1979, 5, 19), null, "bar3");
+
+            var result = await connection.ReadAsync("select * from test_class2").Select<TestClass2>().ToListAsync();
+
+            AssertTestClass2(result);
+
+            await connection.ExecuteAsync("drop table test_class2;");
+        }
+
     }
 }
