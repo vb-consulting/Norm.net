4b32d6336042f72c5e682185588e69ec9ce9ee0f
Vedran Bilopavlović <vbilopav@gmail.com>
2019-12-14 12:36:01 +0100

    Add more PostgreSQL extensions

--- Norm.Extensions.PostgreSQL/Extensions.cs
+++ Norm.Extensions.PostgreSQL/Extensions.cs
@@ -1,21 +1,23 @@
 ﻿using System;
 using System.Data;
 using System.Data.Common;
-using System.Threading.Tasks;
+
 
 namespace Norm.Extensions.PostgreSQL
 {
     public static class Extensions
     {
-        public static string GetSchema(this DbConnection connection) => connection.Single<string>("select current_schema()");
+        public static string CurrentSchema(this DbConnection connection) => connection.Single<string>(Sql.CurrentSchema);
 
-        public static long GetTransactionId(this DbConnection connection) => connection.Single<long>("select txid_current()");
+        public static long TransactionId(this DbConnection connection) => connection.Single<long>(Sql.TransactionId);
 
-        public static long GetBackendPid(this DbConnection connection) => connection.Single<long>("select pg_backend_pid()");
+        public static long BackendPid(this DbConnection connection) => connection.Single<long>(Sql.BackendPid);
 
-        public static DbConnection UseSchema(this DbConnection connection, string schema)
+        public static string TrySearchPathSchema(this DbConnection connection, string schema) => connection.Execute(Sql.SearchPathSchema(schema)).CurrentSchema();
+
+        public static DbConnection SearchPathSchema(this DbConnection connection, string schema)
         {
-            var current = connection.Execute($"set search_path to {schema}").GetSchema();
+            var current = connection.Execute(Sql.SearchPathSchema(schema)).CurrentSchema();
             if (current == null)
             {
                 throw new DataException($"Could not switch schema to {schema}. It may not exists. Use create schema statement.");
@@ -24,24 +26,20 @@ namespace Norm.Extensions.PostgreSQL
         }
 
         public static bool TableExists(this DbConnection connection, string table, string schema = null) =>
-            connection.Single(@$"
-                select 1 
-                from 
-                    information_schema.tables
-                where 
-                    table_name = @table
-                {(schema != null ? "and table_schema = @schema" : "")}
-                ",table, schema).Count != 0;
+            connection.Single(Sql.TableExists(schema), table, schema).Count != 0;
 
-        public static DbConnection Begin(this DbConnection connection) => connection.Execute("begin");
+        public static DbConnection Begin(this DbConnection connection) => connection.Execute(Sql.Begin);
 
-        public static DbConnection End(this DbConnection connection) => connection.Execute("end");
+        public static DbConnection End(this DbConnection connection) => connection.Execute(Sql.End);
 
-        public static DbConnection Savepoint(this DbConnection connection, string name) => connection.Execute($"savepoint {name}");
+        public static DbConnection Savepoint(this DbConnection connection, string name) => connection.Execute(Sql.Savepoint(name));
 
-        public static DbConnection ReleaseSavepoint(this DbConnection connection, string name) => connection.Execute($"release savepoint {name}");
+        public static DbConnection ReleaseSavepoint(this DbConnection connection, string name) => connection.Execute(Sql.ReleaseSavepoint(name));
 
         public static DbConnection Rollback(this DbConnection connection, string savepoint = null) => 
-            connection.Execute(savepoint == null ? "rollback" : $"rollback to {savepoint}");
+            connection.Execute(Sql.Rollback(savepoint));
+
+        public static DbConnection Script(this DbConnection connection, string content, params (string, object)[] parameters) =>
+            connection.Execute(Sql.Script(content, parameters));
     }
 }
---
+++ Norm.Extensions.PostgreSQL/Sql.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Norm.Extensions.PostgreSQL
+{
+    public static class Sql
+    {
+        public static string CurrentSchema => "select current_schema()";
+        public static string TransactionId => "select txid_current()";
+        public static string BackendPid => "select pg_backend_pid()";
+        public static string SearchPathSchema(string schema) => $"set search_path to {schema}";
+        public static string Begin => "begin";
+        public static string End => "end";
+        public static string Savepoint(string name) => $"savepoint {name}";
+        public static string ReleaseSavepoint(string name) => $"release savepoint {name}";
+        public static string Rollback(string savepoint = null) => savepoint == null ? "rollback" : $"rollback to {savepoint}";
+
+        public static string TableExists(string schema = null) => @$"
+                select 1
+                from
+                    information_schema.tables
+                where
+                    table_name = @table
+                {(schema != null ? "and table_schema = @schema" : "")}";
+
+
+        public static string Script(string content, params (string, object)[] parameters) =>
+            @$"
+            do $$
+            {string.Join(string.Concat(";", Environment.NewLine), parameters.Select(t => $"declare {t.Item1}{(t.Item2 == null ? ";" : $" = {t.Item2};")}"))}
+            begin
+                {content}
+            end
+            $$";
+
+        public static string Info(string info, params string[] parameters) =>
+            $"raise info {(parameters.Length == 0 ? string.Concat("'", info, "'") : string.Concat("'", info, "', ", string.Join(", ", parameters)))}";
+    }
+}
