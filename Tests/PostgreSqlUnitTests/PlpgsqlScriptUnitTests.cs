5b58623b90a0d71373a873325caf4e8f617689ca
Vedran Bilopavlović <vbilopav@gmail.com>
2020-03-05 13:19:14 +0100

    PlpgsqlScriptUnitTests

---
+++ Tests/PostgreSqlUnitTests/PlpgsqlScriptUnitTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Norm.Extensions;
+using Npgsql;
+using Xunit;
+
+namespace PostgreSqlUnitTests
+{
+    [Collection("PostgreSqlDatabase")]
+    public class PlpgsqlScriptUnitTests
+    {
+        private readonly PostgreSqlFixture fixture;
+
+        public PlpgsqlScriptUnitTests(PostgreSqlFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public void SimpleScript_Test()
+        {
+            using var connection = new NpgsqlConnection(fixture.ConnectionString)
+                .Execute("create table plpgsql_test1 (t text)")
+                .Execute(@"
+
+                    do $$
+                    begin
+
+                        insert into plpgsql_test1 values ('foo1');
+                        insert into plpgsql_test1 values ('foo2');
+
+                    end
+                    $$ language plpgsql;
+
+                ");
+
+            var result = connection
+                .Read<string>("select t from plpgsql_test1")
+                .ToArray();
+
+            Assert.Equal("foo1", result[0]);
+            Assert.Equal("foo2", result[1]);
+        }
+
+        [Fact]
+        public void ParametersInScript_Test()
+        {
+            using var connection = new NpgsqlConnection(fixture.ConnectionString)
+                .Execute("create table plpgsql_test2 (t text)")
+                .Execute($@"
+
+                    do $$
+                    begin
+
+                        insert into plpgsql_test2 values (format('%s', '{"foo1"}'));
+                        insert into plpgsql_test2 values (format('%s', '{"foo2"}'));
+
+                    end
+                    $$ language plpgsql;
+
+                ");
+
+            var result = connection
+                .Read<string>("select t from plpgsql_test2")
+                .ToArray();
+
+            Assert.Equal("foo1", result[0]);
+            Assert.Equal("foo2", result[1]);
+        }
+    }
+}
