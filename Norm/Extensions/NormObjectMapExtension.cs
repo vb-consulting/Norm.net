52bbc70f266293a15c52af6e5de35d6e7836528b
Vedran Bilopavlović <vbilopav@gmail.com>
2020-05-13 15:03:28 +0200

    Add snake case naming and arrays support to object mapper

--- Norm/Extensions/NormObjectMapExtension.cs
+++ Norm/Extensions/NormObjectMapExtension.cs
@@ -21,9 +21,9 @@ namespace Norm.Extensions
             HashSet<byte> nullableHash,
             bool isNewEntry)
         {
-            foreach (var ((name, value), index) in tuples.Select((item, index) => (item, (byte)index)))
+            foreach (var ((name, value), index) in tuples.Select((item, index) => ((item.name.Replace("_", ""), item.value), (byte)index)))
             {
-                void SetNullableValue<TProp>(TProp propertyValue) where TProp : struct
+                void SetValue<TProp>(TProp propertyValue) where TProp : struct
                 {
                     if (typeDict.TryGetValue(index, out var cachedSetter))
                     {
@@ -59,6 +59,24 @@ namespace Norm.Extensions
                     }
                 }
 
+                void SetSpanValue<TProp>(ReadOnlySpan<TProp> propertyValue) //where TProp : struct
+                {
+                    if (typeDict.TryGetValue(index, out var cachedSetter))
+                    {
+                        ((Action<T, TProp[]>)cachedSetter).Invoke(instance, propertyValue.ToArray());
+                        return;
+                    }
+                    var propertyInfo = instanceType.GetProperty(name, BindingFlags.IgnoreCase | BindingFlags.GetProperty | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+                    if (propertyInfo == null)
+                    {
+                        return;
+                    }
+                    var reflectionSetter = propertyInfo.GetSetMethod(true);
+                    var setter = Delegate.CreateDelegate(typeof(Action<T, TProp[]>), reflectionSetter);
+                    typeDict.Add(index, setter);
+                    ((Action<T, TProp[]>)setter).Invoke(instance, propertyValue.ToArray() as TProp[]);
+                }
+
                 void SetStringValue(ReadOnlySpan<char> propertyValue)
                 {
                     if (typeDict.TryGetValue(index, out var cachedSetter))
@@ -77,53 +95,81 @@ namespace Norm.Extensions
                     ((Action<T, string>)setter).Invoke(instance, propertyValue.ToString());
                 }
 
-                var code = Type.GetTypeCode(value.GetType());
+                TypeCode code;
+                bool isArray;
+                var type = value.GetType();
+                if (type.IsArray)
+                {
+                    isArray = true;
+                    code = Type.GetTypeCode(type.GetElementType());
+                }
+                else
+                {
+                    isArray = false;
+                    code = Type.GetTypeCode(type);
+                }
+
                 switch (code)
                 {
                     case TypeCode.Boolean:
-                        SetNullableValue((bool)value);
+                        //SetNullableValue((bool)value);
+                        if (!isArray) SetValue((bool)value); else SetSpanValue<bool>(new ReadOnlySpan<bool>((bool[])value));
                         continue;
                     case TypeCode.Byte:
-                        SetNullableValue((byte)value);
+                        //SetNullableValue((byte)value);
+                        if (!isArray) SetValue((byte)value); else SetSpanValue<byte>(new ReadOnlySpan<byte>((byte[])value));
                         continue;
                     case TypeCode.Char:
-                        SetNullableValue((char)value);
+                        //SetNullableValue((char)value);
+                        if (!isArray) SetValue((char)value); else SetSpanValue<char>(new ReadOnlySpan<char>((char[])value));
                         continue;
                     case TypeCode.DateTime:
-                        SetNullableValue((DateTime)value);
+                        //SetNullableValue((DateTime)value);
+                        if (!isArray) SetValue((DateTime)value); else SetSpanValue<DateTime>(new ReadOnlySpan<DateTime>((DateTime[])value));
                         continue;
                     case TypeCode.Decimal:
-                        SetNullableValue((decimal)value);
+                        //SetNullableValue((decimal)value);
+                        if (!isArray) SetValue((decimal)value); else SetSpanValue<decimal>(new ReadOnlySpan<decimal>((decimal[])value));
                         continue;
                     case TypeCode.Double:
-                        SetNullableValue((double)value);
+                        //SetNullableValue((double)value);
+                        if (!isArray) SetValue((double)value); else SetSpanValue<double>(new ReadOnlySpan<double>((double[])value));
                         continue;
                     case TypeCode.Int16:
-                        SetNullableValue((short)value);
+                        //SetNullableValue((short)value);
+                        if (!isArray) SetValue((short)value); else SetSpanValue<short>(new ReadOnlySpan<short>((short[])value));
                         continue;
                     case TypeCode.Int32:
-                        SetNullableValue((int)value);
+                        //SetNullableValue((int) value);
+                        if (!isArray) SetValue((int) value); else SetSpanValue<int>(new ReadOnlySpan<int>((int[])value));
                         continue;
                     case TypeCode.Int64:
-                        SetNullableValue((long)value);
+                        //SetNullableValue((long)value);
+                        if (!isArray) SetValue((long)value); else SetSpanValue<long>(new ReadOnlySpan<long>((long[])value));
                         continue;
                     case TypeCode.SByte:
-                        SetNullableValue((sbyte)value);
+                        //SetNullableValue((sbyte)value);
+                        if (!isArray) SetValue((sbyte)value); else SetSpanValue<sbyte>(new ReadOnlySpan<sbyte>((sbyte[])value));
                         continue;
                     case TypeCode.Single:
-                        SetNullableValue((float)value);
+                        //SetNullableValue((float)value);
+                        if (!isArray) SetValue((float)value); else SetSpanValue<float>(new ReadOnlySpan<float>((float[])value));
                         continue;
                     case TypeCode.String:
-                        SetStringValue((string) value);
+                        //SetSpanValue<char>((string) value);
+                        if (!isArray) SetStringValue((string)value); else SetSpanValue<string>(new ReadOnlySpan<string>((string[])value));
                         continue;
                     case TypeCode.UInt16:
-                        SetNullableValue((ushort)value);
+                        //SetNullableValue((ushort)value);
+                        if (!isArray) SetValue((ushort)value); else SetSpanValue<ushort>(new ReadOnlySpan<ushort>((ushort[])value));
                         continue;
                     case TypeCode.UInt32:
-                        SetNullableValue((uint)value);
+                        //SetNullableValue((uint)value);
+                        if (!isArray) SetValue((uint)value); else SetSpanValue<uint>(new ReadOnlySpan<uint>((uint[])value));
                         continue;
                     case TypeCode.UInt64:
-                        SetNullableValue((ulong)value);
+                        //SetNullableValue((ulong)value);
+                        if (!isArray) SetValue((ulong)value); else SetSpanValue<ulong>(new ReadOnlySpan<ulong>((ulong[])value));
                         continue;
                 }
             }
--- Tests/PostgreSqlUnitTests/ObjectMappingUnitTests.cs
+++ Tests/PostgreSqlUnitTests/ObjectMappingUnitTests.cs
@@ -23,6 +23,24 @@ namespace PostgreSqlUnitTests
             public string Bar { get; private set; }
         }
 
+        class SnakeCaseMapTestClass
+        {
+            public int MyId { get; private set; }
+            public string MyFoo { get; private set; }
+            public DateTime MyDay { get; private set; }
+            public bool? MyBool { get; private set; }
+            public string MyBar { get; private set; }
+        }
+
+        class ArraysTestClass
+        {
+            public int[] Id { get; private set; }
+            public string[] Foo { get; private set; }
+            public DateTime[] Day { get; private set; }
+            public bool[] Bool { get; private set; }
+            public string[] Bar { get; private set; }
+        }
+
         private const string Query = @"
                             select *
                             from (
@@ -30,6 +48,30 @@ namespace PostgreSqlUnitTests
                                 (1, 'foo1', '1977-05-19'::date, true, null),
                                 (2, 'foo2', '1978-05-19'::date, false, 'bar2'),
                                 (3, 'foo3', '1979-05-19'::date, null, 'bar3')
+                            ) t(id, foo, day, bool, bar)";
+
+        private const string SnakeCaseQuery = @"
+                            select *
+                            from (
+                            values 
+                                (1, 'foo1', '1977-05-19'::date, true, null),
+                                (2, 'foo2', '1978-05-19'::date, false, 'bar2'),
+                                (3, 'foo3', '1979-05-19'::date, null, 'bar3')
+                            ) t(my_id, my_foo, my_day, my_bool, my_bar)";
+
+        private const string ArraysQuery = @"
+                            select 
+                                array_agg(id) as id,
+                                array_agg(foo) as foo,
+                                array_agg(day) as day,
+                                array_agg(bool) as bool,
+                                array_agg(bar) as bar
+                            from (
+                            values 
+                                (1, 'foo1', '1977-05-19'::date, true, null),
+                                (2, 'foo2', '1978-05-19'::date, false, 'bar2'),
+                                (3, 'foo3', '1979-05-19'::date, null, 'bar3'),
+                                (null, null, null, null, null)
                             ) t(id, foo, day, bool, bar)";
 
         public ObjectMappingUnitTests(PostgreSqlFixture fixture)
@@ -87,6 +129,69 @@ namespace PostgreSqlUnitTests
             var result = await connection.ReadAsync(Query).Select<TestClass>().ToListAsync();
 
             AssertTestClass(result);
+        }
+
+        [Fact]
+        public void SelectSnakeCaseMap_Sync()
+        {
+            using var connection = new NpgsqlConnection(fixture.ConnectionString);
+            var result = connection.Read(SnakeCaseQuery).Select<SnakeCaseMapTestClass>().ToList();
+
+            Assert.Equal(3, result.Count);
+
+            Assert.Equal(1, result[0].MyId);
+            Assert.Equal(2, result[1].MyId);
+            Assert.Equal(3, result[2].MyId);
+
+            Assert.Equal("foo1", result[0].MyFoo);
+            Assert.Equal("foo2", result[1].MyFoo);
+            Assert.Equal("foo3", result[2].MyFoo);
+
+            Assert.Equal(new DateTime(1977, 5, 19), result[0].MyDay);
+            Assert.Equal(new DateTime(1978, 5, 19), result[1].MyDay);
+            Assert.Equal(new DateTime(1979, 5, 19), result[2].MyDay);
+
+            Assert.Equal(true, result[0].MyBool);
+            Assert.Equal(false, result[1].MyBool);
+            Assert.Null(result[2].MyBool);
+
+            Assert.Null(result[0].MyBar);
+            Assert.Equal("bar2", result[1].MyBar);
+            Assert.Equal("bar3", result[2].MyBar);
+        }
+
+        [Fact]
+        public void SelectArraysMap_Sync()
+        {
+            using var connection = new NpgsqlConnection(fixture.ConnectionString);
+            var result = connection.Read(ArraysQuery).Select<ArraysTestClass>().ToList();
+
+            Assert.Single(result);
+
+            Assert.Equal(1, result[0].Id[0]);
+            Assert.Equal(2, result[0].Id[1]);
+            Assert.Equal(3, result[0].Id[2]);
+            Assert.Equal(0, result[0].Id[3]);
+
+            Assert.Equal("foo1", result[0].Foo[0]);
+            Assert.Equal("foo2", result[0].Foo[1]);
+            Assert.Equal("foo3", result[0].Foo[2]);
+            Assert.Null(result[0].Foo[3]);
+
+            Assert.Equal(new DateTime(1977, 5, 19), result[0].Day[0]);
+            Assert.Equal(new DateTime(1978, 5, 19), result[0].Day[1]);
+            Assert.Equal(new DateTime(1979, 5, 19), result[0].Day[2]);
+            Assert.Equal(new DateTime(), result[0].Day[3]);
+
+            Assert.Equal(true, result[0].Bool[0]);
+            Assert.Equal(false, result[0].Bool[1]);
+            Assert.Equal(false, result[0].Bool[2]);
+            Assert.Equal(false, result[0].Bool[3]);
+
+            Assert.Null(result[0].Bar[0]);
+            Assert.Equal("bar2", result[0].Bar[1]);
+            Assert.Equal("bar3", result[0].Bar[2]);
+            Assert.Null(result[0].Bar[3]);
         }
     }
 }
