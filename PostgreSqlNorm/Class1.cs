ec5b28b4f9aaea7a1425b72246ed107fb09f093e
Vedran Bilopavlović <vbilopav@gmail.com>
2020-12-11 13:37:02 +0100

    add unit test

--- PostgreSqlNorm/Class1.cs
+++ PostgreSqlNorm/Class1.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using Npgsql;
+using Norm;
 
 namespace PostgreSqlNorm
 {
     public class Class1
     {
+        public Class1()
+        {
+            using var connection = new NpgsqlConnection("");
+            connection.Execute("");
+        }
     }
 }
