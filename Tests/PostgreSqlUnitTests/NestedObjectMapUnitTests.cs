83def09266e6d4e886acc45ea6e62cfb3be5bbdb
vbilopav <vbilopav@gmail.com>
2022-10-17 10:25:01 +0200

    update nested object test

--- Tests/PostgreSqlUnitTests/NestedObjectMapUnitTests.cs
+++ Tests/PostgreSqlUnitTests/NestedObjectMapUnitTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Norm;
 using Npgsql;
 using Xunit;
@@ -50,73 +51,64 @@ namespace PostgreSqlUnitTests
                                 (4, 'account4', 'addr4', 'country4', 2), 
                                 (5, 'account5', 'addr5', 'country5', 2)");
 
-            Dictionary<int, Shop> shops = new();
-            Dictionary<int, Account> accounts = new();
-
-            foreach (var (shop, account) in connection.Read<Shop, Account>(@"
+            var shops = connection.Read<Shop, Account>(@"
                 select s.*, a.*
                 from shop s 
-                inner join account a on s.id = a.shop_id"))
-            {
-                accounts[account.Id] = account;
-                if (shops.TryGetValue(shop.Id, out var existing))
+                inner join account a on s.id = a.shop_id")
+                .GroupBy(item => item.Item1.Id)
+                .Select(group =>
                 {
-                    existing.Accounts.Add(account);
-                    account.Shop = existing;
-                }
-                else
-                {
-                    shop.Accounts = new List<Account> { account };
-                    shops.Add(shop.Id, shop);
-                    account.Shop = shop;
-                }
-            }
+                    var shop = group.First().Item1;
+                    shop.Accounts = group.Select(item =>
+                    {
+                        var account = item.Item2;
+                        account.Shop = shop;
+                        return account;
+                    }).ToList();
+                    return shop;
+                })
+                .ToList();
 
             Assert.Equal(2, shops.Count);
-            Assert.Equal(5, accounts.Count);
 
-            Assert.Equal("shop1", shops[1].Name);
-            Assert.Equal("shop2", shops[2].Name);
+            Assert.Equal(1, shops[0].Id);
+            Assert.Equal(2, shops[1].Id);
+            Assert.Equal("shop1", shops[0].Name);
+            Assert.Equal("shop2", shops[1].Name);
 
-            Assert.Equal("account1", accounts[1].Name);
-            Assert.Equal("account2", accounts[2].Name);
-            Assert.Equal("account3", accounts[3].Name);
-            Assert.Equal("account4", accounts[4].Name);
-            Assert.Equal("account5", accounts[5].Name);
+            Assert.Equal(3, shops[0].Accounts.Count);
+            Assert.Equal(2, shops[1].Accounts.Count);
 
-            Assert.Equal("addr1", accounts[1].Address);
-            Assert.Equal("addr2", accounts[2].Address);
-            Assert.Equal("addr3", accounts[3].Address);
-            Assert.Equal("addr4", accounts[4].Address);
-            Assert.Equal("addr5", accounts[5].Address);
+            Assert.Equal("account3", shops[0].Accounts[0].Name);
+            Assert.Equal("account2", shops[0].Accounts[1].Name);
+            Assert.Equal("account1", shops[0].Accounts[2].Name);
+            Assert.Equal("addr3", shops[0].Accounts[0].Address);
+            Assert.Equal("addr2", shops[0].Accounts[1].Address);
+            Assert.Equal("addr1", shops[0].Accounts[2].Address);
+            Assert.Equal("country3", shops[0].Accounts[0].Country);
+            Assert.Equal("country2", shops[0].Accounts[1].Country);
+            Assert.Equal("country1", shops[0].Accounts[2].Country);
 
-            Assert.Equal("country1", accounts[1].Country);
-            Assert.Equal("country2", accounts[2].Country);
-            Assert.Equal("country3", accounts[3].Country);
-            Assert.Equal("country4", accounts[4].Country);
-            Assert.Equal("country5", accounts[5].Country);
+            Assert.Equal(shops[0], shops[0].Accounts[0].Shop);
+            Assert.Equal(shops[0], shops[0].Accounts[1].Shop);
+            Assert.Equal(shops[0], shops[0].Accounts[2].Shop);
 
-            Assert.Equal(1, accounts[1].ShopId);
-            Assert.Equal(1, accounts[2].ShopId);
-            Assert.Equal(1, accounts[3].ShopId);
-            Assert.Equal(2, accounts[4].ShopId);
-            Assert.Equal(2, accounts[5].ShopId);
+            Assert.Equal(3, shops[0].Accounts[0].Shop.Accounts.Count);
+            Assert.Equal(3, shops[0].Accounts[1].Shop.Accounts.Count);
+            Assert.Equal(3, shops[0].Accounts[2].Shop.Accounts.Count);
 
-            Assert.Equal(shops[1], accounts[1].Shop);
-            Assert.Equal(shops[1], accounts[2].Shop);
-            Assert.Equal(shops[1], accounts[3].Shop);
-            Assert.Equal(shops[2], accounts[4].Shop);
-            Assert.Equal(shops[2], accounts[5].Shop);
+            Assert.Equal("account5", shops[1].Accounts[0].Name);
+            Assert.Equal("account4", shops[1].Accounts[1].Name);
+            Assert.Equal("addr5", shops[1].Accounts[0].Address);
+            Assert.Equal("addr4", shops[1].Accounts[1].Address);
+            Assert.Equal("country5", shops[1].Accounts[0].Country);
+            Assert.Equal("country4", shops[1].Accounts[1].Country);
 
-            Assert.Equal(3, shops[1].Accounts.Count);
-            Assert.Equal(2, shops[2].Accounts.Count);
+            Assert.Equal(shops[1], shops[1].Accounts[0].Shop);
+            Assert.Equal(shops[1], shops[1].Accounts[1].Shop);
 
-            Assert.Equal(accounts[3], shops[1].Accounts[0]);
-            Assert.Equal(accounts[2], shops[1].Accounts[1]);
-            Assert.Equal(accounts[1], shops[1].Accounts[2]);
-
-            Assert.Equal(accounts[5], shops[2].Accounts[0]);
-            Assert.Equal(accounts[4], shops[2].Accounts[1]);
+            Assert.Equal(2, shops[1].Accounts[0].Shop.Accounts.Count);
+            Assert.Equal(2, shops[1].Accounts[1].Shop.Accounts.Count);
 
             connection
                 .Execute("rollback");
