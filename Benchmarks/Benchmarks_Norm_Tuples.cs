825fd75158474b8d6c01b78ad00859b309012454
Vedran B <vbilopav@gmail.com>
2023-12-03 13:31:53 +0100

    fold tuples benchmark

--- Benchmarks/Benchmarks_Norm_Tuples.cs
+++ Benchmarks/Benchmarks_Norm_Tuples.cs
@@ -5,7 +5,17 @@ public partial class Benchmarks
     [BenchmarkDotNet.Attributes.Benchmark()]
     public void Norm_Tuples()
     {
-        foreach (var i in connection.Read<int, string, string, DateTime, int, string, string, DateTime, string, bool>(query))
+        foreach (var i in connection.Read<
+            int, 
+            string, 
+            string, 
+            DateTime, 
+            int, 
+            string, 
+            string, 
+            DateTime, 
+            string, 
+            bool>(query))
         {
             var c = i;
         }
